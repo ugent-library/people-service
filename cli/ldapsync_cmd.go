1881a896eaae69d8002cfa56045b923f57756aba
Nicolas Franck <nicolas.franck@ugent.be>
2024-01-09 21:27:32 +0100

    remove ldaptest

--- cli/ldapsync_cmd.go
+++ cli/ldapsync_cmd.go
@@ -2,10 +2,7 @@ package cli
 
 import (
 	"context"
-	"fmt"
-	"os"
 
-	"github.com/go-ldap/ldap/v3"
 	"github.com/spf13/cobra"
 	"github.com/ugent-library/people-service/ldapsync"
 )
@@ -28,30 +25,6 @@ var ldapSyncCmd = &cobra.Command{
 	},
 }
 
-var ldapTestCmd = &cobra.Command{
-	Use:   "ldaptest",
-	Short: "ldaptest",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		ctx, cancel := context.WithCancel(context.TODO())
-		defer cancel()
-
-		i := 0
-		err := newUgentLdapClient().SearchPeople(ctx, ldapsync.PersonQuery, func(e *ldap.Entry) error {
-			i++
-			fmt.Fprintf(os.Stderr, "LDAP ENTRY: %d\n", i)
-			for _, attr := range e.Attributes {
-				for _, val := range attr.Values {
-					fmt.Fprintf(os.Stderr, "  %s : %s\n", attr.Name, val)
-				}
-			}
-			return nil
-		})
-		fmt.Fprintf(os.Stderr, "error: %+v\n", err)
-		return err
-	},
-}
-
 func init() {
 	rootCmd.AddCommand(ldapSyncCmd)
-	rootCmd.AddCommand(ldapTestCmd)
 }
