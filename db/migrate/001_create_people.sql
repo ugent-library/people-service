6d5e3bd184aa822cedf6bacf9019fc4336f06605
nsteenla <nicolas.steenlant@ugent.be>
2024-01-25 17:18:40 +0100

    people table

---
+++ db/migrate/001_create_people.sql
@@ -0,0 +1,25 @@
+CREATE TABLE people (
+  id BIGSERIAL PRIMARY KEY,
+  active BOOLEAN DEFAULT false NOT NULL,
+  name TEXT NOT NULL CONSTRAINT CHECK (name <> ''),
+  preferred_name TEXT,
+  given_name TEXT,
+  family_name TEXT,
+  preferred_given_name TEXT,
+  preferred_family_name TEXT,
+  honorific_prefix TEXT,
+  email TEXT,
+  created_at TIMESTAMPTZ NOT NULL,
+  updated_at TIMESTAMPTZ NOT NULL
+);
+
+CREATE TABLE people_identifiers (
+  person_id BIGINT REFERENCES people,
+  type TEXT CONSTRAINT CHECK (type <> ''),
+  value TEXT CONSTRAINT CHECK (value <> ''),
+  PRIMARY KEY (type, value)
+);
+
+---- create above / drop below ----
+
+DROP TABLE IF EXISTS people CASCADE;
