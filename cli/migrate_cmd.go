82e6598cf140c4bed62e083ffba703bc74a45060
nsteenla <nicolas.steenlant@ugent.be>
2024-02-06 17:23:58 +0100

    simplify migrate

--- cli/migrate_cmd.go
+++ cli/migrate_cmd.go
@@ -7,27 +7,22 @@ import (
 	"github.com/ugent-library/people-service/db"
 )
 
-var (
-	migrateUpFlag   bool
-	migrateDownFlag bool
-)
-
 func init() {
-	migrateCmd.Flags().BoolVar(&migrateUpFlag, "up", false, "migrate up")
-	migrateCmd.Flags().BoolVar(&migrateDownFlag, "down", false, "migrate down")
-	migrateCmd.MarkFlagsOneRequired("up", "down")
-	migrateCmd.MarkFlagsMutuallyExclusive("up", "down")
 	rootCmd.AddCommand(migrateCmd)
 }
 
 var migrateCmd = &cobra.Command{
-	Use:   "migrate",
-	Short: "Run database migrations",
-	Args:  cobra.NoArgs,
+	Use:       "migrate",
+	Short:     "Run database migrations",
+	Args:      cobra.ExactArgs(1),
+	ValidArgs: []string{"up", "down"},
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if migrateUpFlag {
+		switch args[0] {
+		case "up":
 			return db.MigrateUp(context.Background(), config.Repo.Conn)
+		case "down":
+			return db.MigrateDown(context.Background(), config.Repo.Conn)
 		}
-		return db.MigrateDown(context.Background(), config.Repo.Conn)
+		return nil
 	},
 }
